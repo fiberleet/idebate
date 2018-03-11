60872c650c3568cca0e4a8e65e07d4600bf30edb
fiberleet <dylanbiddle@gmail.com>
2018-03-11 01:59:12 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'users/show'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#index"
+  devise_for :users, skip: :all
+  resources :users
+end
